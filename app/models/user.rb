b65110e98a12d90e84d4bb6ab81f6b56affe82e1
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-16 19:40:15 -0800

    Added User authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_secure_password
+
 	validates :password, length: { minimum: 3, too_short: ' has to be more than %{count} ' }
 	validates :email, :username, presence: true, uniqueness: true
 	validates :password, presence: true
